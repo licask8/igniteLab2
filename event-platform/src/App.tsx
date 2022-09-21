18ee2c27f23e91d76b225f75f2637be0a0a6945a
elielson <elielson.elrick@gmail.com>
2022-09-21 17:18:01 -0300

    finalizando estrutura visual

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,11 +1,16 @@
-import { Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from  'react-router-dom'
+import { Router } from "./Router";
 
 
 export default function App() {
   return (
-   <div>
-    <Event />
-   </div>
+   <ApolloProvider client={client}>
+      <BrowserRouter >
+         <Router />
+      </BrowserRouter>
+   </ApolloProvider>
   )
 }
 
--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
@@ -17,25 +18,25 @@ export function Lesson({ title, slug, availableAt, type}: LessonProps) {
 
 
     return (
-        <a href="#">
+        <Link to={`/event/lesson/${slug}`} className='group'>
             <span className="text-gray-300">
                 {availableDateFormatted}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
                 <header className="flex items-center justify-between">
 
-                {isLessonAvaiable ? (
-                       <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-                            <CheckCircle size={20} />
-                            Conteúdo liberado
-                         </span>
-                 ): (
-                    <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-                        <Lock size={20} />
-                        Em breve
-                    </span>
-                 )}
+                    {isLessonAvaiable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                                <CheckCircle size={20} />
+                                Conteúdo liberado
+                            </span>
+                    ): (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
 
                     <span className="text-xs rounded py-[2px] px-2 text-white border border-green-300 font-bold">
                         {type === 'live' ? 'AO VIVO' : "AULA PRÁTICA" }
@@ -46,7 +47,7 @@ export function Lesson({ title, slug, availableAt, type}: LessonProps) {
                     {title}
                 </strong>
             </div>
-        </a>
+        </Link>
     );
 } 
 
